dc55c0c66fde3616c9068d604bb3b051172e3b72
Mahmoud Hussein <mahmoud.hussain@golive-solutions.com>
2025-04-28 19:21:45 +0000

    fix: remove item events

--- salla_client/salla_utils.py
+++ salla_client/salla_utils.py
@@ -188,3 +188,120 @@ def update_merchant_requests(**args):
     last_row.consumed_requests = args_dict.consumed_requests
     last_row.remaining_requests = args_dict.remaining_requests
     merchant.save()
+
+def update_salla_price(item_price):
+    settings = get_api_settings()
+
+    data = {
+        "site": settings["site"],
+        "data": str(item_price.as_dict()),
+        "function": "update_salla_price",
+    }
+    try:
+
+        response = requests.post(
+            settings["url"], headers=settings["headers"], json=data
+        )
+
+        response.raise_for_status()
+
+        if response.ok:
+            frappe.msgprint(_("Sent to server"))
+
+    except requests.exceptions.HTTPError as e:
+        frappe.log_error(f"Failed to update Salla price: {str(e)}", "Salla API Error")
+        frappe.throw(_("Failed to send data to server. Please check logs."))
+
+def update_item_barcode(doc):
+    old_doc = doc.get_doc_before_save()
+    if not old_doc or len(doc.barcodes) != len(old_doc.barcodes):
+        barcodes_list = [row.barcode for row in doc.barcodes]
+        if len(barcodes_list) > 0:
+            doc.custom_concatenated_barcode = "#"
+            doc.custom_concatenated_barcode += "#".join(barcodes_list)
+            doc.custom_concatenated_barcode += "#"
+
+
+def Update_salla_online_qty(doc):
+    for salla_item_info in doc.custom_salla_item:
+        update_online_qty(salla_item_info)
+
+
+def update_online_qty(doc):
+    total_online_qty = 0
+
+    if doc.update_online_qty and not doc.parent.is_bundle:
+        doc.update_online_qty = 0
+        # Check for normal Items
+        items = frappe.get_list(
+            "Salla Order Item",
+            filters=[
+                ["is_document_submitted", "=", 0],
+                ["merchant", "=", doc.merchant],
+                ["order_status", "!=", "ملغي"],
+                ["item_code", "=", doc.parent.item_code],
+            ],
+            fields=["qty"],
+        )
+        # frappe.msgprint('Non Bunsle items length is : ' + str(len(items)));
+        if len(items) > 0:
+            for item in items:
+                total_online_qty = total_online_qty + item.qty
+
+        # Check for Item in bundles
+        bundel_items = frappe.get_list(
+            "Salla Order Item",
+            filters=[
+                ["is_document_submitted", "=", 0],
+                ["merchant", "=", doc.merchant],
+                ["order_status", "!=", "ملغي"],
+                ["is_bundle", "=", 1],
+            ],
+            fields=["qty", "barcode", "merchant"],
+        )
+        # frappe.msgprint('bundel_items length is : ' + str(len(bundel_items)));
+        if len(bundel_items) > 0:
+            for bundel_item in bundel_items:
+                # frappe.msgprint('bundle barcode is : '+ bundel_item.barcode);
+                # frappe.msgprint('bundle qty is : '+ str(bundel_item.qty));
+                # frappe.msgprint('total_online_qty is ' + str(total_online_qty));
+                barcodeList = bundel_item.barcode.split("-")
+                for barcode in barcodeList:
+                    # frappe.msgprint('item barcode is : '+ barcode);
+                    # frappe.msgprint('Item to be updated barcode is : '+ doc.barcode);
+                    if "#" + barcode + "#" in doc.parent.barcode:
+                        # frappe.msgprint('barcode is in doc item');
+                        total_online_qty = total_online_qty + bundel_item.qty
+                        # frappe.msgprint('total_online_qty is ' + str(total_online_qty));
+        doc.pending_online_quantity = total_online_qty
+        # frappe.msgprint('total_online_qty is ' + str(total_online_qty));
+        # frappe.msgprint('pending_online_quantity is ' + str(doc.pending_online_quantity));
+
+
+def setup_variant_data(doc, send_to_salla, merchant_name):
+    frappe.msgprint("Setting up default data for the variant")
+    doc.custom_is_salla_item = 1
+    if send_to_salla:
+        doc.custom_send_item_to_salla = 1
+        add_merchant(doc, merchant_name)
+        frappe.msgprint("Merchant and Salla settings applied")
+
+    add_barcode(doc)
+
+
+def add_merchant(doc, merchant_name):
+    if not any(row.merchant == merchant_name for row in doc.custom_salla_item):
+        doc.append("custom_salla_item", {"merchant": merchant_name})
+        frappe.msgprint(f"Merchant {merchant_name} added to variant.")
+
+
+def add_barcode(doc):
+    barcode_value = doc.item_code
+    if not any(barcode.barcode == barcode_value for barcode in doc.barcodes):
+        doc.append("barcodes", {"barcode": barcode_value, "custom_is_salla_barcode": 1})
+        frappe.msgprint(f"Barcode {barcode_value} added to variant.")
+
+
+def has_barcode(doc):
+    """Checks if a barcode already exists in the item variant."""
+    return any(barcode_entry.barcode for barcode_entry in doc.barcodes)
