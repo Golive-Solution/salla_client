b7784f62f4e9e20422402a26c4320fb632848105
Ahmed Hammadi <ahmed.hammadi@golive-solutions.com>
2025-06-11 12:16:44 +0000

    fix:requirements

--- setup.py
+++ setup.py
@@ -15,5 +15,8 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=install_requires
+    install_requires=["salla-common-lib>=0.0.20",
+                      "requests>=2.31.0",
+                      "python-dateutil>=2.8.2",
+                      "PyJWT>=2.8.0" ]
 ) 
