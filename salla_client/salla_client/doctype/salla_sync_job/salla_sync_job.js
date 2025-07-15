b61f940def5201995589ab33d53344606ff3235b
Mahmoud Soliman <mahmoud.soliman@golive-solutions.com>
2025-07-15 16:01:29 +0000

    Hide the Butoon To Prevent Multible Clicks

--- salla_client/salla_client/doctype/salla_sync_job/salla_sync_job.js
+++ salla_client/salla_client/doctype/salla_sync_job/salla_sync_job.js
@@ -4,12 +4,18 @@
 frappe.ui.form.on('Salla Sync Job', {
 	update_bulk_warehouse: function (frm) {
 		if (!(frm.doc.warehouse || frm.doc.merchant)) return;
+		frm.set_df_property('update_bulk_warehouse', 'hidden', 1);
+		frm.refresh_field('update_bulk_warehouse');
 
 		frappe.call({
 			method: "salla_client.utils.update_product_balance_warehouse",
 			args: {
 				merchant_name: frm.doc.merchant, 
 				is_bulk: 1
+			},
+			callback: function(r){
+				frm.set_df_property('update_bulk_warehouse', 'hidden', 0);
+				frm.refresh_field('update_bulk_warehouse');
 			}
 		});
 	}
